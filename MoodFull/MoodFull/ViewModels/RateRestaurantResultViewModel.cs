fb9b4a19477aa851ba4a252c98ac1944ff4420a9
rimwme <tinginuxas09@gmail.com>
2019-11-12 22:43:07 +0200

    Added unfinished layout of RateRestaurantResult page, added ActivityIndicator, Restaurant service, which returns restaurants(mockup right now)

--- MoodFull/MoodFull/ViewModels/RateRestaurantResultViewModel.cs
+++ MoodFull/MoodFull/ViewModels/RateRestaurantResultViewModel.cs
@@ -1,19 +1,30 @@
 ﻿using MoodFull.Interfaces;
 using MoodFull.Models;
 using MoodFull.MoodDetectors;
+using MoodFull.Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MoodFull.ViewModels
 {
-    public class RateRestaurantResultViewModel
+    public class RateRestaurantResultViewModel : INotifyPropertyChanged
     {
         private byte[] faceImage;
         private IMoodDetector moodDetector;
 
         private MoodModel moodModel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<Restaurant> RestaurantList { get; set; }
+
+        public Command GetEmotionsCommand { get; }
 
         public RateRestaurantResultViewModel(byte[] faceImage)
         {
@@ -21,15 +32,63 @@ namespace MoodFull.ViewModels
             moodDetector = new AzureMoodDetector();
 
             //Get Emotions button clicked
-            GetEmotionsCommand = new Command(async () =>
-            {
-                //gets MoodModel with all moods from moodDetector
-                moodModel = await moodDetector.GetEmotions(faceImage);
-                var smth = moodModel;
-            });
+            GetEmotionsCommand = new Command(async ()=> await GetMood(), () => !IsWaiting);
+
+            RestaurantList = RestaurantService.GetRestaurants().OrderBy(c => c.Name).ToList();
         }
 
-        public Command GetEmotionsCommand { get; }
+        private Restaurant selectedRestaurant;
+        public Restaurant SelectedRestaurant
+        {
+            get
+            {
+                return selectedRestaurant;
+            }
+            set
+            {
+                SetProperty(ref selectedRestaurant, value);
+            }
+        }
 
+        private bool isWaiting = false;
+        public bool IsWaiting
+        {
+            get
+            {
+                return isWaiting;
+            }
+            set
+            {
+                isWaiting = value;
+                OnPropertyChanged();
+                GetEmotionsCommand.ChangeCanExecute();
+            }
+        }
+
+        async Task GetMood()
+        {
+            IsWaiting = true;
+            //for whatever reason without Task.Delay(1) the ActivityIndicator isn't working
+            await Task.Delay(1);
+            moodModel = await moodDetector.GetEmotions(faceImage);
+            IsWaiting = false;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName =null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T backfield, T value,
+            [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backfield, value))
+            {
+                return false;
+            }
+            backfield = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
