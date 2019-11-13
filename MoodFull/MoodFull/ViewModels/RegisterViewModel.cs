47e53c5f357b4fb607c3c4da6d51b1bf9336321e
maob5579 <mantas.obolevicius@mif.stud.vu.lt>
2019-11-13 16:49:25 +0200

    Sukurta register forma. Pats varotojo sukurimas dar neveikia. Reikes kurti kai susiejim su DB

---
+++ MoodFull/MoodFull/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MoodFull.Views;
+using Xamarin.Forms;
+
+namespace MoodFull.ViewModels
+{
+    class RegisterViewModel
+    {
+        //klase kuri sukurs nauja vartotojo paskyra.  Paspaudus register sugrazins kontrole
+        //i login langa
+
+        string username;
+        string password;
+        string confirmPassword;
+        string name;
+        string lastName;
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string ConfirmPassword { get => confirmPassword; set => confirmPassword = value; }
+        public string Name { get => name; set => name = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+        public Command LauchRegisterWindowCommand { get; }
+
+        public RegisterViewModel()
+        {
+            LauchRegisterWindowCommand = new Command(async () =>
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new LoginPage());
+            });
+        }
+
+    }
+}
