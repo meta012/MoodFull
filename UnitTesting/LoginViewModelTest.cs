56b065fc8a9a01e534173e7e8eb7d20c48e7c7c2
maob5579 <mantas.obolevicius@mif.stud.vu.lt>
2019-11-16 22:57:40 +0200

    Mainly kol kas tik susiconfiginau test projekta. Kol kas prideti 2 testai. Reiketu daugiau data validation metodu projektuose.

---
+++ UnitTesting/LoginViewModelTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoodFull.ViewModels;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UnitTesting
+{
+    class LoginViewModelTest
+    {
+        [Test]
+        public void UserPropertyUserNameGetSetWorks()
+        {
+            LoginViewModel model = new LoginViewModel();
+            model.Username = null;
+            Assert.AreEqual(model.Username, null);
+        }
+
+        [Test]
+        public void UserPropertyPasswordGetSetWorks()
+        {
+            LoginViewModel model = new LoginViewModel();
+            model.Password = null;
+            Assert.AreEqual(model.Password, null);
+        }
+
+    }
+}
