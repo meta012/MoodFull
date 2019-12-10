05e0a4200e79d30d5f293f18e59584b4355d7162
ginterma <maciukonis1@gmail.com>
2019-12-10 22:28:13 +0200

    Added functionality and design to RatedRestaurants View

--- MoodFull/MoodFull/Views/RatedRestaurants.xaml.cs
+++ MoodFull/MoodFull/Views/RatedRestaurants.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MoodFull.ViewModels;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -17,5 +18,31 @@ namespace MoodFull.Views
 			InitializeComponent ();
 			BindingContext = new ViewModels.RatedRestaurantsViewModel();
 		}
-	}
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var vm = BindingContext as RatedRestaurantsViewModel;
+            MergedListListsView.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                MergedListListsView.ItemsSource = vm.MergedList;
+            else
+                MergedListListsView.ItemsSource = vm.MergedList.Where(i => i.RestaurantName.ToLower().Contains(e.NewTextValue.ToLower()));
+
+            MergedListListsView.EndRefresh();
+
+        }
+        private void SearchBar_OnTextChanged1(object sender, TextChangedEventArgs e)
+        {
+            var vm = BindingContext as RatedRestaurantsViewModel;
+            MergedListListsView.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                MergedListListsView.ItemsSource = vm.MergedList;
+            else
+                MergedListListsView.ItemsSource = vm.MergedList.Where(i => i.Username.ToLower().Contains(e.NewTextValue.ToLower()));
+
+            MergedListListsView.EndRefresh();
+
+        }
+    }
 }
